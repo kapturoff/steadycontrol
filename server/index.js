244eb96ecd223653d5ffeb847261ea5f24eb801d
kapturoff <pigunderwater@gmail.com>
2022-11-17 14:35:00 +0300

    Add simple cors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const Koa = require('koa'),
-    { server } = require('../package.json')
+    { server } = require('../package.json'),
+    cors = require('@koa/cors')
 
 const app = new Koa({}),
     router = require('./routes/router')
 
+app.use(cors({ origin: '*' }))
 app.use(router.routes())
 
 app.listen(server.port, () => {

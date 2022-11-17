b3683d35a54543646f715c52b3f04bc92ac6f8ff
kapturoff <pigunderwater@gmail.com>
2022-11-17 18:08:13 +0300

    Send built frontend from server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,18 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    build: {
+        outDir: '../server/static'
+    },
+    server: {
+        proxy: {
+            '/tree': 'http://localhost:8080/tree/'
+        }
     }
-  }
 })
--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const Koa = require('koa'),
     { server } = require('../package.json'),
-    cors = require('@koa/cors')
+    cors = require('@koa/cors'),
+    serve = require('koa-static'),
+    { join } = require('path')
 
 const app = new Koa({}),
-    router = require('./routes/router')
+    router = require('./routes/router'),
+    staticPath = join(__dirname, 'static')
 
+app.use(serve(staticPath))
 app.use(cors({ origin: '*' }))
 app.use(router.routes())
 
